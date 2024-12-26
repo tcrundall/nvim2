94ac438e51a22c7ef1e791f32dc04ca963fcf35b
Timothy Crundall <tim.crundall@gmail.com>
2024-12-26 17:50:12 +0100

    Configure blink completion to use luasnips

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -1,7 +1,38 @@
 return {
   "Saghen/blink.cmp",
   tag = "v0.8.2",
-  config = function()
-    require("blink.cmp").setup({})
-  end,
+  -- config = function()
+  --   require("blink.cmp").setup({})
+  -- end,
+  -- !Important! Make sure you're using the latest release of LuaSnip
+  -- `main` does not work at the moment
+  dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+  opts = {
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
+    sources = {
+      default = { "lsp", "path", "luasnip", "buffer" },
+    },
+    keymap = {
+      -- Add custom mappings for snippets
+      ["<C-l>"] = { "snippet_forward", "fallback" },
+      ["<C-h>"] = { "snippet_backward" },
+
+      -- Remove default mappings for tab
+      ["<Tab>"] = { "fallback" },
+      ["<S-Tab>"] = { "fallback" },
+    },
+  },
 }
