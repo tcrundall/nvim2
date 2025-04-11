24b4e9ee8a91882d1b46fbd4269050c9129e7c29
Timothy Crundall <tim.crundall@gmail.com>
2025-04-11 16:58:41 +0200

    Tweak harpoon maps

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -18,7 +18,7 @@ return {
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end)
-    vim.keymap.set("n", "<C-e>", function()
+    vim.keymap.set("n", "<leader><tab>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
