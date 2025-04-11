64177ababb1386b759748719b2095643cd328e21
Timothy Crundall <tim.crundall@gmail.com>
2025-04-11 16:58:57 +0200

    Improve colorscheme
    * add catppuccin
    * load tokyonight on startup and first

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,6 +1,9 @@
 return {
+  "catppuccin/nvim", -- for my favourite daymode, catppuccin-frappe
   {
     "folke/tokyonight.nvim",
+    lazy = false, -- load on startup
+    priority = 1000, -- load first
     config = function()
       vim.cmd.colorscheme("tokyonight")
     end,
