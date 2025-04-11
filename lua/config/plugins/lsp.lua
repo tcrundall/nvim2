b7b4729aca5ada824d0d8a15fd932db49eeb7659
Timothy Crundall <tim.crundall@gmail.com>
2025-04-11 17:34:42 +0200

    Indent lsp plugins as prep for more lsps

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,56 +1,58 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "saghen/blink.cmp" },
-    {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "saghen/blink.cmp" },
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
     },
-  },
-  opts = {
-    servers = {
-      lua_ls = {},
-      gopls = {},
+    opts = {
+      servers = {
+        lua_ls = {},
+        gopls = {},
+      },
     },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
+
+          -- if client:supports_method('textDocument/completion') then
+          --   -- Enable auto-completion
+          --   vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+          -- end
+
+          if client:supports_method("textDocument/formatting", 0) then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+    end,
   },
-  config = function(_, opts)
-    local lspconfig = require("lspconfig")
-    for server, config in pairs(opts.servers) do
-      -- passing config.capabilities to blink.cmp merges with the capabilities in your
-      -- `opts[server].capabilities, if you've defined it
-      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
-    end
-
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client == nil then
-          return
-        end
-
-        -- if client:supports_method('textDocument/completion') then
-        --   -- Enable auto-completion
-        --   vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
-        -- end
-
-        if client:supports_method("textDocument/formatting", 0) then
-          -- Format the current buffer on save
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = args.buf,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-            end,
-          })
-        end
-      end,
-    })
-  end,
 }
