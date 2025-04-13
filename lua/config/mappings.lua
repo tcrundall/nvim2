e1ce22c837334d2438e625a5667e15aa6c44a54b
Timothy Crundall <tim.crundall@gmail.com>
2025-04-13 17:11:21 +0200

    Add tmux navigator

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -7,11 +7,6 @@ map("v", "<space>x", ":lua<cr>")
 map("n", "<A-u>", "<cmd>cnext<cr>")
 map("n", "<A-i>", "<cmd>cprev<cr>")
 
-map("n", "<C-j>", "<C-W>j")
-map("n", "<C-k>", "<C-W>k")
-map("n", "<C-h>", "<C-W>h")
-map("n", "<C-l>", "<C-W>l")
-
 map("n", "<esc>", "<cmd>noh<cr>")
 
 map("t", "<esc><esc>", "<C-\\><C-N>")
---
+++ lua/config/plugins/tmuxnavigator.lua
@@ -0,0 +1,14 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = true,
+  keys = {
+    { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+  config = function()
+    vim.api.nvim_exec2("let g:tmux_navigator_disable_when_zoomed = 1", {})
+  end,
+}
