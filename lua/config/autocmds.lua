e992e61b5eeceedb1b344a4588c2b05e21bd7553
Timothy Crundall <tim.crundall@gmail.com>
2025-04-13 19:25:26 +0200

    Add folding with fancy auto commands to autoexpand

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,28 +1,49 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    if vim.opt_local.foldmethod ~= "manual" then
-      return
-    end
-
-    if require("nvim-treesitter.parsers").has_parser() then
-      print("setting fold method: expr")
-      vim.opt_local.foldmethod = "expr"
-      vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-      -- vim.opt.foldmethod = "expr"
-      -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    else
-      print("setting fold method: syntax")
-      -- vim.opt.foldmethod = "indent"
-      vim.opt_local.foldmethod = "indent"
-    end
-    vim.opt.foldenable = false
-  end,
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight text on yank",
   group = vim.api.nvim_create_augroup("text-yank-highlight", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWrite", {
+  desc = "Store folds before saving",
+  group = vim.api.nvim_create_augroup("save-folds-on-write", {}),
+  command = "mkview",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  desc = "Load folds after saving",
+  group = vim.api.nvim_create_augroup("load-folds-after-save", {}),
+  command = "silent! loadview",
+})
+
+-- tracking how often because only want to do this once per file
+-- and "NewBuf" doesn't cut it
+local times_opened = {}
+vim.api.nvim_create_autocmd("BufEnter", {
+  desc = "Ensure folds are open",
+  group = vim.api.nvim_create_augroup("PreOpenFolds", {}),
+  callback = function()
+    local filename = vim.fn.expand("%:p")
+    if times_opened[filename] == nil then
+      times_opened[filename] = 0
+    end
+    times_opened[filename] = times_opened[filename] + 1
+    if times_opened[filename] ~= 2 then
+      -- For some reason, this event gets triggered twice
+      -- and only on the second trigger does the set fold level persist
+      return
+    end
+
+    -- An equivalent-ish version of vim.fn.execute(":let &foldlevel = max(map(range(1, line('$')), 'foldlevel(v:val)'))")
+    local line_count = vim.api.nvim_buf_line_count(0)
+    local max_fold_level = 0
+    for line_number = 1, line_count do
+      local line_fold_level = vim.treesitter.foldexpr(line_number)
+      line_fold_level = line_fold_level:gsub(">", "") + 0
+      max_fold_level = math.max(max_fold_level, line_fold_level)
+    end
+    vim.opt.foldlevel = max_fold_level
   end,
 })
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -14,4 +14,9 @@ set.scrolloff = 8
 -- keep sign column on to avoid jerking around
 set.signcolumn = "yes"
 
+-- if closing without right, prompt for resolution
 set.confirm = true
+
+-- set fold method based treesitter parsing
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
