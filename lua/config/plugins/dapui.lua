346585a786d195ce65d944650e74652edba4629d
Timothy Crundall <tim.crundall@gmail.com>
2025-06-15 16:16:15 +0200

    Open dapui upon start and close upon termination

--- lua/config/plugins/dapui.lua
+++ lua/config/plugins/dapui.lua
@@ -96,5 +96,9 @@ return {
     vim.keymap.set("n", "<F7>", function()
       require("dapui").toggle()
     end)
+
+    require("dap").listeners.after.event_initialized["dapui_config"] = require("dapui").open
+    require("dap").listeners.before.event_terminated["dapui_config"] = require("dapui").close
+    require("dap").listeners.before.event_exited["dapui_config"] = require("dapui").close
   end,
 }
