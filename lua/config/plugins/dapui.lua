60813b8aaa237dc5edee0cb254dceb308f45f74e
Timothy Crundall <tim.crundall@gmail.com>
2025-06-15 15:25:56 +0200

    Configure dap ui

---
+++ lua/config/plugins/dapui.lua
@@ -0,0 +1,94 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    require("dapui").setup({
+      controls = {
+        element = "repl",
+        enabled = true,
+        icons = {
+          disconnect = "",
+          pause = "",
+          play = "",
+          run_last = "",
+          step_back = "",
+          step_into = "",
+          step_out = "",
+          step_over = "",
+          terminate = "",
+        },
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = "single",
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+      force_buffers = true,
+      icons = {
+        collapsed = "",
+        current_frame = "",
+        expanded = "",
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = "watches",
+              size = 0.8,
+            },
+            {
+              id = "repl",
+              size = 0.2,
+            },
+            -- {
+            --   id = "console",
+            --   size = 0.3,
+            -- },
+          },
+          position = "bottom",
+          size = 10,
+        },
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+          },
+          position = "left",
+          size = 40,
+        },
+      },
+      mappings = {
+        edit = "e",
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        repl = "r",
+        toggle = "t",
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100,
+      },
+    })
+
+    vim.keymap.set("n", "<F7>", function()
+      require("dapui").toggle()
+    end)
+  end,
+}
