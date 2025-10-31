d6bca00354c79786ec2bcad47acc2a6c9312be28
Timothy Crundall <tim.crundall@gmail.com>
2025-10-31 12:26:00 +0100

    Delete superfluous gisigns plugin initialization

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -66,23 +66,6 @@ return {
     opts = {},
   },
   {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "_" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
-          untracked = { text = "┆" },
-        },
-        current_line_blame = true,
-        max_file_length = 5000,
-      })
-    end,
-  },
-  {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
     opts = {
