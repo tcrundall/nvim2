8cdc18d47bce506886233734844808a186124112
Timothy Crundall <tim.crundall@gmail.com>
2025-11-02 20:58:25 +0100

    Add mappings for project specific notes

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -17,6 +17,19 @@ map("n", "#", "#zz")
 local log_file = vim.env.GTD_LOG_FILE or "~/log.md"
 map("n", "<leader>el", "<cmd>vs " .. log_file .. "<cr>")
 
+local proj_notes_file = vim.env.PROJ_NOTES_FILE or "~/notes-project.md"
+map("n", "<leader>epn", "<cmd>vs " .. proj_notes_file .. "<cr>")
+
+local proj_log_file = vim.env.PROJ_LOG_FILE or "~/log-project.md"
+map("n", "<leader>epl", "<cmd>vs " .. proj_log_file .. "<cr>")
+
 map("n", "y%", function()
   vim.fn.setreg('"', vim.fn.expand("%"))
 end)
+
+-- TODO: Attach these mappings to a group activated and deactivated via user command
+-- -- wrapped editing
+-- map("n", "j", "gj")
+-- map("n", "k", "gk")
+-- map("n", "$", "g$")
+-- map("n", "0", "g0")
