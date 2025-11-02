cd49fb182e8a841b933c16bc7ba99a533aef50b8
Timothy Crundall <tim.crundall@gmail.com>
2025-11-02 20:54:24 +0100

    Add user command for rendering local openapi files

--- lua/config/usercommands.lua
+++ lua/config/usercommands.lua
@@ -1,1 +1,13 @@
 vim.api.nvim_create_user_command("W", "w", { desc = "Write even if command typo" })
+
+local openApiViewer = function(opts)
+  local filename = vim.fn.expand("%:t")  -- 't' = only file name
+  local extension = vim.fn.expand("%:e") -- 'e' = only extension
+  local filenameWithoutExtension = filename:gsub("." .. extension, "")
+  vim.fn.execute("!openapi-viewer % &")
+  vim.fn.execute("!open http://localhost:3000/" .. filenameWithoutExtension .. " &")
+end
+
+vim.api.nvim_create_user_command("OpenApiViewer", function(opts)
+  openApiViewer(opts)
+end, {})
