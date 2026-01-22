d78bc07f09da65bb3b335d483de1b6379f360df9
Timothy Crundall <tim.crundall@gmail.com>
2026-01-22 21:23:29 +0100

    WIP: AI fix code-companion chat

--- lua/config/plugins/code-companion.lua
+++ lua/config/plugins/code-companion.lua
@@ -8,30 +8,36 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    strategies = {
-      chat = {
-        adapter = "my_openai",
-        model = vim.env.AI_INLINE_MODEL,
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "my_openai",
+        },
+        inline = {
+          adapter = "my_openai",
+        },
+        cmd = {
+          adapter = "my_openai",
+        },
       },
-      inline = {
-        adapter = "my_openai",
-        model = vim.env.AI_INLINE_MODEL,
+      adapters = {
+        http = {
+          my_openai = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = {
+                url = vim.env.AI_URL,
+                api_key = vim.env.AI_API_KEY,
+              },
+              schema = {
+                model = {
+                  default = vim.env.AI_INLINE_MODEL or "gpt-4",
+                },
+              },
+            })
+          end,
+        },
       },
-      cmd = {
-        adapter = "my_openai",
-        model = vim.env.AI_INLINE_MODEL,
-      },
-    },
-    adapters = {
-      my_openai = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url = vim.env.AI_URL,
-            api_key = vim.env.AI_API_KEY,
-          },
-        })
-      end,
-    },
-  },
+    })
+  end,
 }
