7cd59a8e6bdf8e660ca4cf996224e27be92ff0a6
Tim Crundall <tim.crundall@gmail.com>
2026-04-12 18:03:30 +0200

    Add execution map for bash

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,3 +1,11 @@
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader><leader>x",
+  "<cmd>w<cr><cmd>!bash %<cr>",
+  { desc = "e[X]ecute current bash file" }
+)
