469353281753fa9fc5a47ae3df703b06fecb7312
Tim Crundall <tim.crundall@gmail.com>
2026-08-07 14:56:37 +0200

    Fix lua file execution

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,5 +3,5 @@ vim.opt_local.expandtab = true
 
 local bmap = vim.api.nvim_buf_set_keymap
 
-bmap(0, "n", "<leader><leader>x", "<cmd>w<cr><cmd>.lua<cr>", { desc = "e[X]ecute current lua file" })
-bmap(0, "v", "<space>x", ":lua<cr>", {})
+bmap(0, "n", "<leader><leader>x", "<cmd>w<cr><cmd>source %<cr>", { desc = "e[X]ecute current lua file" })
+bmap(0, "v", "<leader>x", ":lua<cr>", {})
