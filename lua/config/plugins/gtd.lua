bf5133564988c7395ff93f60146f61378c6c05b6
Tim Crundall <tim.crundall@gmail.com>
2026-08-07 15:02:06 +0200

    Add in basket file creater

--- lua/config/plugins/gtd.lua
+++ lua/config/plugins/gtd.lua
@@ -1,6 +1,18 @@
 local notes_dir = vim.env.NOTES_DIR
 local next_actions_file = vim.env.NEXT_ACTIONS_FILE
 
+local function add_to_in_basket()
+  vim.ui.input({ prompt = "topic" }, function(input)
+    local filename = notes_dir .. "/_in-basket/" .. input:lower():gsub(" ", "-") .. ".md"
+    local title = "# " .. input
+    vim.fn.writefile({ title }, filename)
+    local bufnr = vim.fn.bufadd(filename)
+    vim.api.nvim_open_win(bufnr, true, { split = "below" })
+  end)
+end
+
+vim.api.nvim_create_user_command("InBasket", add_to_in_basket, {})
+
 return {
   "tcrundall/gtd.nvim",
   opts = {
@@ -11,5 +23,6 @@ return {
     { "<leader>ea", "<cmd>vsplit " .. next_actions_file .. "<cr>", mode = "n" },
     { "<a-t>", "<cmd>GtdToggleTargetAction<cr>", mode = "n" },
     { "<a-c>", "<cmd>GtdToggleCheck<cr>", mode = "n" },
+    { "<leader>ei", "<cmd>InBasket<cr>", mode = "n" },
   },
 }
